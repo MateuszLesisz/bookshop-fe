0175ce9bba7fd6782f0291956d10dab7fa3db8a3
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2024-01-04 14:02:52 +0100

    build basket (logic)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {CatalogService} from './catalog.service';
-import {Book} from "./book";
-import {HttpErrorResponse} from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { CatalogService } from './catalog.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from "./book";
+import { catchError, tap } from 'rxjs/operators';
+import { CartItem } from "./cartitem";
 
 @Component({
   selector: 'app-root',
@@ -11,23 +14,40 @@ import {HttpErrorResponse} from "@angular/common/http";
 export class AppComponent implements OnInit {
   title = 'frontakroo';
   public catalog: Book[];
+  public cartItem: CartItem[] = [];
 
-  constructor(private catalogService: CatalogService) {}
+  constructor(private catalogService: CatalogService, private http: HttpClient) {}
 
-  public getCatalog(): void {
-    this.catalogService.getCatalog().subscribe(
-      (response: Book[]) => {
+  ngOnInit(): void {
+    this.getCatalog().toPromise();
+  }
+
+  getCatalog(): Observable<Book[]> {
+    return this.catalogService.getCatalog().pipe(
+      tap((response: Book[]) => {
         console.log("Got book list:");
         console.log(response);
         this.catalog = response;
-      },
-      (error: HttpErrorResponse) => {
+      }),
+      catchError((error: HttpErrorResponse) => {
         console.error(error.message);
-      }
+        throw error; // Rzucenie błędu, aby przekazać go dalej
+      })
     );
   }
 
-  ngOnInit(): void {
-    this.getCatalog();
+  public addToCart(book: Book): void {
+    const current: CartItem = this.cartItem.find(x => x.bookId == book.id)!;
+    if (current) {
+      current.quantity += 1;
+    } else {
+      this.cartItem.push({bookId: book.id, quantity: 1} as CartItem);
+    }
+    console.log('Cart is' + JSON.stringify(this.cartItem));
+  }
+
+  public countCartItems(): number {
+    return this.cartItem
+    .reduce((sum, current) => sum + current.quantity, 0)
   }
 }
